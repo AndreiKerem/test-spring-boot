c6d09b21ff30a7ac647cd6c0c478f53f72a39fd9
AndreiKerem <andres.stan69@gmail.com>
2024-06-10 00:45:57 +0300

    Added the PDF file.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,39 @@
 package com.example.demo;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.font.Standard14Fonts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
 
+        try {
+			PDDocument document = new PDDocument();
+			PDPage page = new PDPage();
+			document.addPage(page);
+
+			PDPageContentStream contentStream = new PDPageContentStream(document, page);
+			contentStream.setFont(new PDType1Font(Standard14Fonts.FontName.COURIER), 14);
+			contentStream.beginText();
+			contentStream.newLineAtOffset(50, 720);
+			contentStream.showText("Hello! This is my first pdf file generated with Java!");
+			contentStream.endText();
+			contentStream.close();
+
+			document.save("myFirstPdfWithJava.pdf");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
+
